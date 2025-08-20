99a00a1adb488716c954269923262e6108230775
Marianne Hval <mariannehval@gmail.com>
2025-08-20 15:18:43 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import ReactDOM from 'react-dom/client';
+import { routeTree } from './routeTree.gen';
+import './styles.css';
+
+// Set up a Router instance
+const router = createRouter({
+	routeTree,
+	defaultPreload: 'intent',
+	defaultStaleTime: 5000,
+	scrollRestoration: true,
+});
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// biome-ignore lint/style/noNonNullAssertion: <Included by default>
+const rootElement = document.getElementById('app')!;
+
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(<RouterProvider router={router} />);
+}
